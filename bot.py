e1a4ee90dbde61f8f6db96e1395822444296113c
clown338 <68019348+clown338@users.noreply.github.com>
2020-09-06 15:44:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -154,55 +154,6 @@ async def avatar(ctx, member : discord.Member = None):
          await ctx.message.delete()
 
 
-@client.command()
-@commands.has_permissions( administrator = True) 
-async def ph( ctx, arg = None):
-	response = requests.get( f'https://htmlweb.ru/geo/api.php?json&telcod={ arg }' )
-
-	user_country = response.json()[ 'country' ][ 'english' ]
-	user_id = response.json()[ 'country' ][ 'id' ]
-	user_location = response.json()[ 'country' ][ 'location' ]
-	user_city = response.json()[ 'capital' ][ 'english' ]
-	user_width = response.json()[ 'capital' ][ 'latitude' ]
-	user_lenth = response.json()[ 'capital' ][ 'longitude' ]
-	user_post = response.json()[ 'capital' ][ 'post' ]
-	user_oper = response.json()[ '0' ][ 'oper' ]
-
-	global all_info
-	all_info = f'''**:iphone: Информация о номере телефона {arg}**
-	**Оператор:** { user_oper }
-	**Страна:** {user_country}
-	**Локация:** { user_location }
-	**Город:** { user_city }
-	**Горизонталь:** { user_width }
-	**Вертикаль:** { user_lenth }
-	**Почтовый индекс:** { user_post }
-	**ID:** { user_id }'''
-
-	await ctx.send( all_info ) 
-	await ctx.message.delete()
-
-
-@client.command()
-async def io( ctx, arg ):
-	response = requests.get( f'http://ipinfo.io/{ arg }/json' )
-
-	user_ip = response.json()[ 'ip' ]
-	user_city = response.json()[ 'city' ]
-	user_region = response.json()[ 'region' ]
-	user_country = response.json()[ 'country' ]
-	user_location = response.json()[ 'loc' ]
-	user_org = response.json()[ 'org' ]
-	user_timezone = response.json()[ 'timezone' ]
-
-	global all_info
-	emb = discord.Embed(title = f'Информация об айпи {arg}:',
-	description = f':united_nations: **Страна**: { user_city }\n\n:regional_indicator_r: **Регион**: { user_region }\n\n:cityscape: **Город**: { user_country }\n\n:map: **Локация**: { user_location }\n\n:bust_in_silhouette: **Организация**: { user_org }\n\n:clock: **Временная зона**: { user_timezone }', colour= 0x39d0d6, inline = False)
-	emb.set_footer(text= "Вызвано: {}".format(ctx.message.author), icon_url= ctx.message.author.avatar_url)
-
-	await ctx.send(embed = emb)
-	await ctx.message.delete()
-
 
 @client.command( pass_context = True, aliases=[ "mutevoice", "мутвойс", "мьютвойс", "Мьютвойс" ] )
 @commands.has_permissions( administrator = True)
@@ -240,80 +191,6 @@ async def __voice_error(ctx, error):
     if isinstance( error, commands.MissingRequiredArgument ):
         await ctx.send(embed = discord.Embed(description = f'**:exclamation: {ctx.author.name},укажите пользователя или время**', color=0x0c0c0c))
 
-
-
-conn = sqlite3.connect("DiscordBot.db") # подключаем таблицу
-cursor = conn.cursor() # управление таблицей
-
-# создаем таблицу если её нету
-cursor.execute("""CREATE TABLE IF NOT EXISTS users (
-                id TEXT,
-                nickname TEXT,
-                mention TEXT,
-                lvl INT,
-                xp INT)""")
-
-
-@client.event
-async def on_message(message):
-    cursor.execute(f"SELECT id FROM users where id={message.author.id}") # загружаем БД игрока
-    if cursor.fetchone() == None: # Если игрока нету в БД, но он на сервере, то..
-        cursor.execute(f"INSERT INTO users VALUES ({message.author.id}, '{message.author.name}', '<@{message.author.id}>', 1, 0)") # вводим данные игрока согласно созданной таблице
-    else: # если игрок есть в БД
-        pass
-    conn.commit() # сохранение БД
-    if len(message.content) > 5: # будем начислять опыт за сообщение больше 5ти символов
-        for row in cursor.execute(f"SELECT xp,lvl FROM users where id={message.author.id}"):
-            expi = row[0]+r.randint(1, 10) # вводим новую переменную, в которую будем добавлять опыт от 1 до 3 единиц
-            cursor.execute(f'UPDATE users SET xp={expi} where id={message.author.id}') # обновляем опыт игрока в БД
-            lvch = expi/(row[1]*100) # при каком количестве будет повышение LVL
-            lv = int(lvch)
-            if row[1] < lv: #если текущий уровень меньше уровня, который был рассчитан формулой выше, то...
-                await message.channel.send(f'Поздравляю! Вы достигли нового уровня!')
-                bal=100*lv
-                cursor.execute(f'UPDATE users SET lvl={lv} where id={message.author.id}') # обновляем уровень игрока
-    await client.process_commands(message) # данная фича нужна чтобы узать потом команды
-    conn.commit() # опять сохраняем БД
-
-
-@client.event
-async def on_member_join(member):
-    cursor.execute(f"SELECT id FROM users where id={member.id}") # все также, существует ли участник в БД
-    if cursor.fetchone() == None:
-        cursor.execute(f"INSERT INTO users VALUES ({member.id}, '{member.name}', '<@{member.id}>', 1, 0)")
-    else:
-        pass
-    conn.commit()
-
-
-@client.command()
-async def lvl(ctx):
-    for row in cursor.execute(f"SELECT lvl,xp FROM users where id={ctx.author.id}"): # задаем цикл где row[0] - уровень, а row[1] - опыт
-        await ctx.send(f'Ваш уровень {row[0]}. Опыт: {row[1]}.')
-
-
-@client.command()
-@commands.cooldown(1, 30*30*16, commands.BucketType.user)
-async def bonus(ctx):
-    for row in cursor.execute(f"SELECT xp FROM users"):
-        xp = row[0]
-        amount = r.randint(20, 50)
-        await ctx.send(embed=discord.Embed(description=f'Вы получили свой бонус в размере {amount} биткоинов!'))
-
-        xp += amount
-
-        cursor.execute(f"UPDATE users SET xp = {xp} WHERE id={ctx.author.id}")
-        conn.commit()
-
-
-@bonus.error
-async def bonus_error(ctx, error):
-    if isinstance(error, commands.CommandOnCooldown):
-        e = discord.Embed(color=0xFF0000)
-        e.add_field(name = 'Кулдаун!', value='У Вас кулдаун на данную команду',inline=False)
-        e.add_field(name = 'Попробуйте ввести команду через -', value='{:.2f} минут.'.format(error.retry_after / 60))
-        e.set_footer(text = f'{client.user.name} © 2020 | Все права защищены', icon_url = client.user.avatar_url)
-    await ctx.send(embed = e)
 
 
 
