a543bce81fb5e30b203f6629223f116ba3ccc585
clown338 <68019348+clown338@users.noreply.github.com>
2020-09-15 23:41:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -281,12 +281,10 @@ async def serverinfo(ctx):
 async def сompatibility(ctx, member : discord.Member):
 
         list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100]
-        e = discord.Embed(colour=0xFEA0FD)
-        e.add_field(name = "**Совместимость**", value = "**Идёт процесс подсчета совместимости **")
-        e.add_field(name = f"**Между {ctx.author.mention} и {member.mention} **")
-        e.add_field(name = f"**Результат:{random.choice(list)} :rat: **")
-        e.set_thumbnail(url = f"https://media.discordapp.net/attachments/644458478601240587/755067305607561256/ds_o_o_ns_s_Instagram_photo-removebg-preview.png?width=468&height=468")
-        await ctx.send(embed = e)
+        embed=discord.Embed(title="Совместимость", description="Идёт подсчёт между пользователями", color=0xcd60aa)
+        embed.set_thumbnail(url="https://media.discordapp.net/attachments/755484420701552800/755485264151052365/jBEoRgekMHI-removebg-preview.png?width=463&height=475")
+        embed.add_field(name="Результат:", value="Совместимость", inline=True)
+        await self.bot.say(embed=embed)
 @client.command()
 @commands.has_permissions( administrator = True)
 async def ban(ctx, member: discord.Member, *, reason = None):
