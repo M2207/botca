4215442e5ad8a738265a66e7f84da2ce9927ba62
clown338 <68019348+clown338@users.noreply.github.com>
2020-09-16 17:14:55 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -55,7 +55,10 @@ async def on_message_delete(message):
     emb.set_footer(text=f'ID Пользователя: {message.author.id} | ID Сообщения: {message.id}')
     await channel.send(embed=emb)
     return
-
+@client.event
+async def on_message(ctx,msg ):
+   if msg.content == " Ок ":
+     await ctx.send(' Хуек ') 
                                          
 @client.event
 async def on_raw_reaction_add(payload):
