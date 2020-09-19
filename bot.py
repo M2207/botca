313ca9f8281e5e75ad20f7d0e8d6975080351197
clown338 <68019348+clown338@users.noreply.github.com>
2020-09-20 00:42:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -284,7 +284,7 @@ async def serverinfo(ctx):
 @client.command(aliases = ["совместимость"])
 async def сompatibility(ctx, member : discord.Member):
 
-        list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100]
+        list = [,100]
         embed=discord.Embed(title="Совместимость :" ,description="**Идёт подсчёт между пользователями**", color=0xcd60aa)
         embed.set_thumbnail(url="https://media.discordapp.net/attachments/755484420701552800/755485264151052365/jBEoRgekMHI-removebg-preview.png?width=463&height=475")
         embed.add_field(name="Результат:", value=f"Совместимость {ctx.author.mention} и {member.mention},** равна {random.choice(list)}  % :rat: ** ", inline=True)
